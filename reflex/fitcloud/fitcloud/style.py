b72c213e1471bfb231481b01806eae1f75fd18b3
woohwan <whpark70@naver.com>
2024-02-28 18:03:48 +0900

    complete basic login UI

---
+++ reflex/fitcloud/fitcloud/style.py
@@ -0,0 +1,13 @@
+login_container_style = dict(
+    width="400px",
+    height="75vh",
+    bg="#1D2330",
+    borderRadius="15px",
+    boxShadow="41px -41px 82px #0d0f15, -41px 41px 82px #2d374b",
+)
+
+main_container_style = dict(
+    width="auto",
+    height="100vh",
+    bg="#1D2330",
+)
