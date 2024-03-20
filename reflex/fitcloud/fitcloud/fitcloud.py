0176ebef9b33f31bf8dd3703ae272a1c2e1079a5
woohwan <whpark70@naver.com>
2024-03-20 21:05:24 +0900

    ui ok

--- reflex/fitcloud/fitcloud/fitcloud.py
+++ reflex/fitcloud/fitcloud/fitcloud.py
@@ -3,9 +3,13 @@ from fitcloud import style
 from fitcloud.state import FormState
 
 def login() -> rx.Component:
-    return rx.chakra.form(
+    return rx.box( 
+        rx.chakra.form(
             rx.chakra.form_control(
                 rx.chakra.vstack(
+                    rx.heading(
+                        "Login into your Account", size="7", margin_bottom="2rem"
+                    ),
                     rx.input(
                         placeholder="Enter userid",
                         id="userId",
@@ -22,16 +26,25 @@ def login() -> rx.Component:
                         id="mfaCode",
                         style=style.input_style,
                     ),
-                    rx.button(
-                        "Submit",
-                        color_scheme="blue",
-                        type="submit",
+                    rx.box(
+                        rx.button(
+                            "Submit",
+                            color_scheme="blue",
+                            type="submit",
+                            style=style.button_style,
+                            margin_top="15px",
+                        ),
                     ),
                 ),
             ),
             on_submit=FormState.handle_submit,
-            reset_on_submit=True,  
-        )
+            reset_on_submit=True,
+        ),
+        padding="4rem 5rem",
+        margin_top="15vh",
+        margin_x="auto",
+        border="1px solid gray"
+    )
 
 def auth():
     pass
--- reflex/fitcloud/fitcloud/style.py
+++ reflex/fitcloud/fitcloud/style.py
@@ -1,5 +1,9 @@
 shadow = "rgba(0, 0, 0, 0.15) 0px 2px 8px"
 
 input_style = dict(
-    border_width="1px", padding="1em", box_shadow=shadow
+    border_width="1px", padding="1em", box_shadow=shadow, width="200px"
+)
+
+button_style = dict(
+    border_width="1px", padding="1em", box_shadow=shadow, width="150px"
 )
